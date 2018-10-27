49d69669eb7c397d8330d60dc42ccd4cad333bff
Erick Cruz <31190144+Hoizard@users.noreply.github.com>
2018-10-27 16:53:39 -0500

    Added basic list for controller

---
+++ You/Controllers/YouController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace You.Controllers
+{
+    public class YouController : Controller
+    {
+        // GET: You
+        public ActionResult Index()
+        {
+            List<string> list = new List<string>
+            {
+                "Erick",
+                "Cruz",
+                "ITMD-563"
+            };
+
+            ViewBag.Message = "My Test View";
+            ViewBag.Names = list;
+            return View();
+        }
+    }
+}
