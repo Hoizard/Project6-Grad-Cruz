f85389a366968653ab5f4a2de8c61575c236d6ad
Erick Cruz <31190144+Hoizard@users.noreply.github.com>
2018-10-28 12:50:44 -0500

    Create controllers and Views but need to work on saving data to display or grabbing dating to output

--- You/Controllers/YouController.cs
+++ You/Controllers/YouController.cs
@@ -11,16 +11,79 @@ namespace You.Controllers
         // GET: You
         public ActionResult Index()
         {
-            List<string> list = new List<string>
-            {
-                "Erick",
-                "Cruz",
-                "ITMD-563"
-            };
-
-            ViewBag.Message = "My Test View";
-            ViewBag.Names = list;
             return View();
+        }
+
+        // GET: You/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: You/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: You/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: You/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: You/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: You/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: You/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ You/Models/Name.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace You.Models
+{
+    public class Name
+    {
+
+        public virtual int PersonId { get; set; }
+
+        [DisplayName("First Name")]
+            [Required]
+            public virtual string FirstName { get; set; }
+
+            [DisplayName("Last Name")]
+            [Required]
+            public virtual string Lastname { get; set; }
+    }
+}
